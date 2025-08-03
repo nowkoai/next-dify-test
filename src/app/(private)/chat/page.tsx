a822855fa5367bcd8cb9465002dfcaaf3e254478
nowkoai <hyamasit@gmail.com>
2025-08-04 01:39:42 +0900

    id

---
+++ src/app/(private)/chat/page.tsx
@@ -0,0 +1,15 @@
+import ChatContainer from "@/components/ChatContainer"
+import { auth } from "@/auth"
+
+export default async function ChatPage() {
+  const session = await auth()
+  const userId = session?.user?.id as string
+
+  return (
+    <ChatContainer 
+      isNewChat={true}
+      initialMessages={[]}
+      conversationId={null}
+      userId={userId} />
+  )
+}
